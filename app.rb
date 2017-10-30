e2041d2e54431eb517087e0e2d5ed5bb69186a78
Fabiano Cunha <cunhasb@aol.com>
2017-10-30 00:26:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Fabiano"
+  end
+
+  get '/hometown' do
+      "My hometown is Red Bank"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is Cathedral"
+    end
 end
